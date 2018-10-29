e12e73487d440e1e11cb37b6f7b80bf5e0e26988
akasata <akasata@rmake.net>
2018-10-29 22:02:42 +0900

    :beer: Counter Componentの実装

---
+++ src/components/Counter.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Counter = ({ value }) => (
+  <div>
+    <h1>{value}</h1>
+    <button>+</button>
+    <button>-</button>
+  </div>
+);
+
+export default Counter;
